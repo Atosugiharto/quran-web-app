4b5f0811ae1c83d19b23e5b8d6157da73ef72157
Ato Sugiharto <atosoegiharto@gmail.com>
2022-07-07 04:04:44 +0700

    update header list

--- src/components/ListSurat.jsx
+++ src/components/ListSurat.jsx
@@ -27,7 +27,7 @@ function ListSurat() {
           <div className="text-lg max-w-prose mx-auto">
             <h1>
               <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
-                Quran Web App
+                QuranWebApp
               </span>
               <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                 DAFTAR SURAH
